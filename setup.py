3ee26c204e1b9b3a7449c7887239d7761cbd5e95
astrobatty <astrobatty@gmail.com>
2022-09-27 11:28:13 +0200

    add setup files

---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from os import path
+
+version = 0.1
+
+# Load requirements
+requirements = None
+with open('requirements.txt') as file:
+    requirements = file.read().splitlines()
+
+# If Python3: Add "README.md" to setup.
+# Useful for PyPI. Irrelevant for users using Python2.
+try:
+    this_directory = path.abspath(path.dirname(__file__))
+    with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+except:
+    long_description = ' '
+
+setup(name='pythia',
+      version=version,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=['pythia.timeseries','pythia.utils'],
+      package_dir = {'pythia.timeseries': 'timeseries','pythia.utils': 'utils'},
+      install_requires=requirements
+     )
